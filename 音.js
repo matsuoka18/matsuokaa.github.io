1193e1fc827e8b22fee425f6f2efbee90daeb81d
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2021-04-17 11:56:38 +0900

    Create 音.js

---
+++ 音.js
@@ -0,0 +1,18 @@
+function ring(){
+  document.getElementById("tts").load();
+  document.getElementById("tts").play();
+};
+
+function stop(){
+  document.getElementById("tts").pause();
+  document.getElementById("ttd").load();
+  document.getElementById("ttd").play();
+}
+function ooe(){
+  document.getElementById("do2").load();
+  document.getElementById("do2").play();
+}
+function cll(){
+document.getElementById("do1").load();
+document.getElementById("do1").play();
+}
